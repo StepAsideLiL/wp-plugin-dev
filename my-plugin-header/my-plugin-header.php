d98d0f55dffaf054fa8170a3eeed5a05b888098f
stepasidelil <rifat6633@gmail.com>
2022-05-06 16:24:45 +0600

    my-plugin-header is added

---
+++ my-plugin-header/my-plugin-header.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       My Plugin Header
+ * Author:            Rifat Khan
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       This plugin shows the metadata of a plugin header comment.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author URI:        https://stepasidelil.hashnode.dev/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-plugin-header
+ * Domain Path:       /languages
+ */
--- my-plugin/README.md
+++ my-plugin/README.md
@@ -1,1 +1,1 @@
-## This is a WordPress example plugin of [this tutorial](https://stepasidelil.hashnode.dev/wordpress-plugin-development).
+## This is a WordPress example plugin of [WordPress Plugin Development](https://stepasidelil.hashnode.dev/wordpress-plugin-development).
